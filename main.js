386eeaf2524108152c2595e4139a7521fc22f59d
pearmini <subairui@icloud.com>
2025-12-02 11:17:00 -0500

    Jiggly Wordle

--- main.js
+++
@@ -1,5 +0,0 @@
-import {render} from "./render.js";
-
-const seed = process.env.NODE_ENV === "development" ? 1744570991909 : Date.now();
-
-document.getElementById("app").appendChild(render({seed}));
